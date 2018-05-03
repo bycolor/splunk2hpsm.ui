777016a076e7c1c5c3b1e165026eb5dca90d2340
bycolor <bycolor@users.noreply.github.com>
2018-05-03 19:05:12 +0300

    2

--- init.js
+++ init.js
@@ -1,6 +1,20 @@
-  var app = new Vue({
-    el: '#app',
+ var appHomepage = new Vue({
+    el: '#header',
     data: {
-      message: 'Hello Vue!'
+      title: 'Splunk 2 HPSM'
     }
-  })
+ });
+
+ var appSplunkForm = new Vue({
+    el: '#splunk-form-holder',
+    data: {
+
+	},
+	methods: {
+		getSplunkSessionkey: function(el) {
+			console.debug('getSplunkSessionkey - el: ', el);
+			debugger;
+			appSplunkForm.$refs.splunkForm.submit();
+		}
+	}
+ });
